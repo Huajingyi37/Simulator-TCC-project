f26f348d4ab7e766028338bdbc056287e7c1ac96
crocmii08-creator <crocmii08@gmail.com>
2026-04-03 10:15:12 +0000

    Add feature X / fix bug Y

--- app.py
+++ app.py
@@ -138,4 +138,4 @@ def dashboard():
                            iot_heatmap_html=iot_heatmap_html)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=5080)
